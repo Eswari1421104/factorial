495deba76b766649ad5883a1d5f17d4f4af4ca4c
Eswari1421104 <eswaribaskar24@gmail.com>
2017-06-14 18:41:04 +0530

    Create Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,13 @@
+package conversion;
+
+import java.io.*;
+import java.util.*;
+public class Conversion
+{
+    public static void main(String args[])
+    {
+      Scanner s=new Scanner(System.in);
+      String ss=s.next();
+      System.out.println(ss);
+    }
+}
